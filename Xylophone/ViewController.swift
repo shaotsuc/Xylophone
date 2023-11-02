430dabb21592432a36174d736bb1b8fea3a91ca7
shaotsu <72166702+shaotsuc@users.noreply.github.com>
2023-11-02 22:18:37 +0200

    Practiced and modified some UI elements.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -4,18 +4,30 @@
 //
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
+//  Practiced and modify by Shao-Tsu on 02/11/2023
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
     
-
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
